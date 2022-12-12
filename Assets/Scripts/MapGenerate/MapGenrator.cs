5c391857116534be4e1c6917ad8547322ce15c1a
Deep3AM <deep3am@gmail.com>
2022-12-13 00:34:00 +0900

    draw line

--- Assets/Scripts/MapGenerate/MapGenrator.cs
+++ Assets/Scripts/MapGenerate/MapGenrator.cs
@@ -6,27 +6,13 @@ public class Node
 {
     [SerializeField] int x;
     [SerializeField] int y;
+    public List<Node> childNodes;
 
     public Node(int x, int y)
     {
         this.x = x;
         this.y = y;
-    }
-}
-
-[System.Serializable]
-public class NodeList
-{
-    public List<Node> nodes;
-
-    public NodeList()
-    {
-        nodes = new List<Node>();
-    }
-
-    public void Add(Node node)
-    {
-        nodes.Add(node);
+        childNodes = new List<Node>();
     }
 }
 
@@ -34,17 +20,20 @@ public class MapGenrator : MonoBehaviour
 {
     [SerializeField] private int width;
     [SerializeField] private int height;
-    [SerializeField] private List<NodeList> nodes = new List<NodeList>();
+    [SerializeField] private List<List<Node>> nodes = new List<List<Node>>();
     [SerializeField] private GameObject pNodeImage;
-    private List<GameObject> nodeImages = new List<GameObject>();
+    [SerializeField] private GameObject pLineImage;
+    private List<List<GameObject>> nodeImages = new List<List<GameObject>>();
+
 
     public void Generate()
     {
         Random.InitState((int)System.DateTime.Now.Ticks);
         nodes.Clear();
-        foreach (GameObject image in nodeImages)
+        foreach (List<GameObject> images in nodeImages)
         {
-            Destroy(image);
+            foreach (GameObject image in images)
+                Destroy(image);
         }
         nodeImages.Clear();
         float heightScale = Screen.height / (float)height;
@@ -55,7 +44,8 @@ public class MapGenrator : MonoBehaviour
             Debug.Log(currentNodetargetNum);
             float widthScale = Screen.width / (float)currentNodetargetNum;
             float startX = widthScale / 2;
-            NodeList currentNodes = new NodeList();
+            List<Node> currentNodes = new List<Node>();
+            List<GameObject> nodeImagesList = new List<GameObject>();
             for (int x = 0; x < currentNodetargetNum; x++)
             {
                 Node newNode = new Node(x, y);
@@ -63,9 +53,42 @@ public class MapGenrator : MonoBehaviour
                 GameObject tempImage = Instantiate(pNodeImage);
                 tempImage.transform.SetParent(this.gameObject.transform);
                 tempImage.transform.position = new Vector2(startX + x * widthScale, startY + y * heightScale);
-                nodeImages.Add(tempImage);
+                nodeImagesList.Add(tempImage);
             }
+            nodeImages.Add(nodeImagesList);
             nodes.Add(currentNodes);
+        }
+        LinkNode();
+    }
+
+    public void LinkNode()
+    {
+        Random.InitState((int)System.DateTime.Now.Ticks);
+        for (int i = 0; i < nodes.Count - 1; i++)
+        {
+            int maxChildNum = nodes[i + 1].Count;
+            for (int j = 0; j < nodes[i].Count; j++)
+            {
+                int randChildNum = Random.Range(1, maxChildNum + 1);
+                List<int> couldBeChildListIdx = new List<int>();
+                for (int idx = 0; idx < maxChildNum; idx++)
+                {
+                    couldBeChildListIdx.Add(idx);
+                }
+                for (int randCountNum = 0; randCountNum < randChildNum; randCountNum++)
+                {
+                    int randIdx = Random.Range(0, couldBeChildListIdx.Count);
+                    nodes[i][j].childNodes.Add(nodes[i + 1][couldBeChildListIdx[randIdx]]);
+                    GameObject line = Instantiate(pLineImage);
+                    line.transform.SetParent(transform);
+                    line.GetComponent<RectTransform>().sizeDelta = new Vector2(Vector2.Distance(nodeImages[i][j].transform.position, nodeImages[i + 1][couldBeChildListIdx[randIdx]].transform.position), 4.5f);
+                    Vector2 tempVector = new Vector2((nodeImages[i][j].transform.position.x + nodeImages[i + 1][couldBeChildListIdx[randIdx]].transform.position.x) / 2, (nodeImages[i][j].transform.position.y + nodeImages[i + 1][couldBeChildListIdx[randIdx]].transform.position.y) / 2);
+                    line.transform.position = tempVector;
+                    Vector2 dir = nodeImages[i][j].transform.position - nodeImages[i + 1][couldBeChildListIdx[randIdx]].transform.position;
+                    line.transform.rotation = Quaternion.FromToRotation(Vector2.right, dir);
+                    couldBeChildListIdx.Remove(couldBeChildListIdx[randIdx]);
+                }
+            }
         }
     }
 }
